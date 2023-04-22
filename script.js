08ad35b2c766a9e0fc42d630f67ba62856f32c76
Michal Pomorski <mipomorski@gmail.com>
2023-04-22 22:12:27 +0100

    many changes add input form

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
 const btn = document.querySelector(".btn-passwords");
+const container = document.querySelector(".passwords-container");
 
 const lettersString = "zxcvbnmlkjhgfdsaqwertyuiop1234567890";
 const lettersAndNumbers = [...lettersString];
-let passwordsToGenerate = 5;
-let passwordLength = 8;
+
+let passwordsToGenerate = 1;
+const passwordsCountInput = document.getElementById("passwords-count");
+passwordsCountInput.addEventListener("change", function (e) {
+  container.textContent = "";
+  if (e.target.value < 1 || e.target.value >= 9999 || e.target.value == "") {
+    document.querySelector(".invalid-input-count").classList.remove("hidden");
+    this.classList.add("invalid");
+    passwordsToGenerate = "";
+  } else {
+    this.classList.remove("invalid");
+    document.querySelector(".invalid-input-count").classList.add("hidden");
+    passwordsToGenerate = e.target.value;
+  }
+});
+
+let passwordLength = 4;
+const passwordsCharsInput = document.getElementById("passwords-chars");
+passwordsCharsInput.addEventListener("change", function (e) {
+  container.textContent = "";
+  if (e.target.value < 4 || e.target.value > 20 || e.target.value === "") {
+    document.querySelector(".invalid-input-chars").classList.remove("hidden");
+    this.classList.add("invalid");
+    passwordLength = "";
+  } else {
+    this.classList.remove("invalid");
+    document.querySelector(".invalid-input-chars").classList.add("hidden");
+    passwordLength = e.target.value;
+  }
+});
+
 let counter = 0;
 btn.addEventListener("click", function () {
+  container.textContent = "";
   for (let i = 0; i < passwordsToGenerate; i++) {
     let passwordArray = [];
     for (let x = 0; x < passwordLength; x++) {
@@ -18,16 +49,16 @@ btn.addEventListener("click", function () {
   }
 });
 
-function passwordElement(item) {
+function passwordElement(item = "") {
   const haslo = document.createElement("div");
   haslo.classList.add("password");
   const joinPassword = item.join("");
-  document.body.appendChild(haslo).innerHTML = `
+  container.appendChild(haslo).innerHTML = `
   
   <p class="text">${joinPassword}</p>
       <button class="copy-btn tooltip" onmouseout="outFunc()">
-        <span class="ph ph-clipboard-text"></span>
-        <span class="tooltiptext myTooltip">Copy to clipboard</span>
+      <span class="ph ph-clipboard-text"></span>
+      <span class="tooltiptext myTooltip">Copy to clipboard</span>
       </button>`;
   readyToCopy();
 }
@@ -37,7 +68,7 @@ function outFunc() {
   tooltip.forEach((e) => (e.textContent = `Copy to clipboard!`));
 }
 
-function readyToCopy(joinPassword) {
+function readyToCopy() {
   const btnCopy = document.querySelectorAll(".copy-btn");
   btnCopy.forEach((e, i) =>
     e.addEventListener("click", function () {
